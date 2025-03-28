7809f8396e339fcfa707a3a749b0b64136134e48
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2025-03-28 11:16:03 -0500

    feat: :sparkles: Javascript promises explanation

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-function abrirVehiculo(marca, callback){
-    setTimeout(function(){
-        console.log("Se abre el vehiculo marca "+marca);
-        callback(marca);
-    }, 1000);
+function abrirVehiculo(marca) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (marca === "Mazda") {
+                console.log("Se abre el vehiculo marca " + marca);
+                resolve(marca);
+            } else {
+                console.log("No es posible abrir el vehiculo marca " + marca);
+                reject("Error al abrir");
+            }
+        }, 1000)
+    });
 }
 
-function encenderVehiculo(marca, callback){
-    setTimeout(function(){
-        console.log("Se enciende el vehiculo marca "+marca);
-        callback();
-    }, 3000);
+function encenderVehiculo(marca) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (marca === "Mazda") {
+                console.log("Se enciende el vehiculo marca " + marca);
+                resolve(marca);
+            } else {
+                console.log("No es posible encender el vehiculo marca " + marca);
+                reject("Error al encender el vehiculo");
+            }
+        }, 1000)
+    });
 }
 
-function encenderLuces(marca, callback){
-    setTimeout(function(){
-        console.log("Se encienden las luces del vehiculo marca "+marca);
-        callback();
-    }, 4000);
+function encenderLuces(marca) {
+    return new Promise(function (res, rej) {
+        setTimeout(function () {
+            console.log("Se encienden las luces del vehiculo " + marca);
+            res();
+        }, 3000);
+    })
 };
 
-abrirVehiculo("Toyota", function(marca){
-    encenderVehiculo(marca, function(){
-        encenderLuces(marca, function(){
-            console.log("Listo para funcionar...");
-        });
+abrirVehiculo("Mazda")
+    .then(encenderVehiculo)
+    .then(encenderLuces)
+    .then(function () {
+        console.log("Estamos ready!!!");
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log("Esto termino bien o mal pero termino");
     });
-});
